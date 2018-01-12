14ed86ecacb2bca483506ea5e22fafe49016872e
nschaetti <n.schaetti@gmail.com>
2018-01-13 00:07:51 +0100

    Vidéo 2 de la chaîne NS.ai

--- Tweet-Classification/create_dataset.py
+++ Tweet-Classification/create_dataset.py
@@ -6,15 +6,16 @@
 import tweepy
 import pickle
 import codecs
+import time
 
 # Republican users
 users = dict()
-users['republicans'] = ["realDonaldTrump", "marcorubio", "JeffFlake", "RandPaul", "lisamurkowski", "SenToomey",
-                        "SenJohnMcCain", "tedcruz", "ChuckGrassley", "SenatorCollins", "SenDeanHeller", "JerryMoran",
-                        "SenatorTimScott", "SenTomCotton", "senrobportman"]
-users['democrats'] = ["HillaryClinton", "BillClinton", "BarackObama", "SenBobCasey", "SenGillibrand", "SenBillNelson",
-                      "SenFeinstein", "SenSchumer", "SenBlumenthal‏", "SenJeffMerkley‏", "SenatorHeitkamp", "SenSanders",
-                      "SenatorTomUdall", "SenBennetCO", "SenWhitehouse"]
+users['democrats'] = [u"HillaryClinton", u"BillClinton", u"BarackObama", u"SenBobCasey", u"SenGillibrand", u"SenBillNelson",
+                      u"SenFeinstein", u"SenSchumer", u"SenMarkey", u"SenatorHeitkamp", u"SenSanders",
+                      u"SenatorTomUdall", u"SenBennetCO", u"SenWhitehouse", u"SenWarren"]
+users['republicans'] = [u"realDonaldTrump", u"marcorubio", u"JeffFlake", u"RandPaul", u"lisamurkowski", u"SenToomey",
+                        u"SenJohnMcCain", u"tedcruz", u"ChuckGrassley", u"SenatorCollins", u"SenDeanHeller", u"JerryMoran",
+                        u"SenatorTimScott", u"SenTomCotton", u"senrobportman"]
 
 
 # Twitter API credentials
@@ -35,14 +36,19 @@ X = list()
 Y = list()
 
 # For republicans and democrats
-for c in users.keys():
+for c in ["democrats", "republicans"]:
+    print(u"Downloading tweets for class \"{}\"".format(c))
+    tweet_text = u""
     for user in users[c]:
+        print(u"Downloading tweets for user \"{}\"".format(user))
         # Get statuses
         for status in api.user_timeline(screen_name=user, count=200):
-            X.append(status.text)
-            Y.append(c)
+            tweet_text += status.text
         # end for
+        X.append(tweet_text)
+        Y.append(c)
+        time.sleep(60)
     # end for
 # end for
 
-pickle.dump((X, Y), codecs.open(u"dataset.p", 'wb'))
+pickle.dump((X, Y), codecs.open(u"dataset.p", 'wb', encoding='utf-8'))
