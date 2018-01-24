577591fcd9149a18c44f06903dc10faebec9a935
Nils Schaetti <n.schaetti@gmail.com>
2018-01-24 16:11:49 +0100

    Function gradient

--- pyTorch1/function_gradient.py
+++ pyTorch1/function_gradient.py
@@ -95,15 +95,11 @@ ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
 # Plot a basic surface.
-ax.plot_surface(X, Y, Z, cmap=cm.hot, linewidth=0, antialiased=True)
-
-# Ensure that the next plot doesn't overwrite the first plot
-ax = plt.gca()
-ax.hold(True)
+ax.plot_surface(X, Y, Z, cmap=cm.hot, linewidth=0, antialiased=True, alpha=0.5)
 
 # Scatter points
 ax.scatter(x_values, y_values, z_values, c='b', marker='o', s=2.0)
-ax.scatter([-1], [0], [-2], c='g', marker='o', s=4.0)
+ax.scatter([-1], [0], [-2], color='g', marker='o', s=4.0)
 
 plt.show()
 
