acf0caaae568625b90d781a1392b5ad49b07c38b
nschaetti <n.schaetti@gmail.com>
2018-01-24 22:45:14 +0100

    Vidéo 2 de la chaîne NS.ai

--- pyTorch1/function_gradient.py
+++ pyTorch1/function_gradient.py
@@ -3,11 +3,9 @@
 #
 
 # Imports
-#from __future__ import print_function
 import torch
 from torch.autograd import Variable
 import argparse
-from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import math
 import numpy as np
